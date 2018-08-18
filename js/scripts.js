94aeba38419557719efcd6421d1259f14bcb3e7d
lorna nashipae <kimanilorna@yahoo.com>
2018-08-18 13:08:41 +0300

    This commit to add functionalitys to my buttons such that when one clicks it shows a div that had initially been hidden

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
+$(document).ready(function() {
+  $("button#start").click(function() {
+    $(".question1").show();
+  });
+});
+
+$(document).ready(function() {
+  $("button#question1").click(function() {
+    $(".question2").show();
+  });
+});
+
+$(document).ready(function() {
+  $("button#question2").click(function() {
+    $(".question3").show();
+  });
+});
+
+
 
 $(document).ready(function(){
   $("form#input").submit(function(event){
     event.preventDefault();
-
-
 var questionOne =  $('input[name="question1"]:checked').val();
 var questionTwo =  $('input[name="question2"]:checked').val();
 var questionThree= $('input[name="question3"]:checked').val();
-
 var result=parseInt(questionOne)+ parseInt(questionTwo)+ parseInt(questionThree);
+alert(+result);
 });
+
 });
