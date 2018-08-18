4213081f62e1e93411859ef5992c995538ed9294
lorna nashipae <kimanilorna@yahoo.com>
2018-08-18 19:02:30 +0300

    This commit is to add css and jQuery features

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 $(document).ready(function() {
   $("button#question1").click(function() {
     $(".question2").show();
+
   });
 });
 
@@ -16,6 +17,17 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function() {
+  $("button#question3").click(function() {
+    $(".question4").show();
+  });
+});
+
+$(document).ready(function() {
+  $("button#question4").click(function() {
+    $(".question5").show();
+  });
+});
 
 
 $(document).ready(function(){
@@ -24,8 +36,20 @@ $(document).ready(function(){
 var questionOne =  $('input[name="question1"]:checked').val();
 var questionTwo =  $('input[name="question2"]:checked').val();
 var questionThree= $('input[name="question3"]:checked').val();
-var result=parseInt(questionOne)+ parseInt(questionTwo)+ parseInt(questionThree);
-alert(+result);
+var questionFour =  $('input[name="question4"]:checked').val();
+var questionFive= $('input[name="question5"]:checked').val();
+var userName= $("input#name").val();
+$(".name").text(userName);
+var result=parseInt(questionOne)+ parseInt(questionTwo)+ parseInt(questionThree)+parseInt(questionFour)+ parseInt(questionFive);
+$(".result").text(result);
+var percentage=(result/25)*100;
+$(".percentage").text(percentage);
+$(".question1").hide();
+$(".question2").hide();
+$(".question3").hide();
+$(".question4").hide();
+$(".question5").hide();
+$("#output").show();
 });
 
 });
